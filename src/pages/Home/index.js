c78d646391c6fa7fe5b942be405ce0d25f0012f8
Elias-exe <adventurergamer11@hotmail.com>
2022-06-02 11:54:03 -0300

    feat: adding the stay list to the card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,64 +1,43 @@
+import { useEffect, useState } from 'react';
 import { Container, CardGroup, Card } from './styles';
+import stays from '../../components/stays.json';
 
 import PageHeader from '../../components/PageHeader';
 import Header from '../../components/Header';
 
 export default function Home() {
+  const [house, setHouse] = useState([]);
+
+  function loadHouses() {
+    setHouse(stays);
+  }
+
+  useEffect(() => {
+    loadHouses();
+  }, [loadHouses]);
+
   return (
     <Container>
       <Header />
       <PageHeader />
 
       <CardGroup>
-        <Card>
-          <img src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80" alt="Hotel" />
-          <div className="infos">
-            <span className="host-tag">SUPER HOST</span>
-            <span>Entire apartment . 2 beds</span>
-            <div className="avaliation">
-              <span className="material-icons">star</span>
-              <span>4.40</span>
+        {house.map((stay) => (
+          <Card key={stay.city}>
+            <img src={stay.photo} alt="Hotel" />
+            <div className="infos">
+              {stay.superHost && (
+                <span className="host-tag">SUPER HOST</span>
+              )}
+              <span>{stay.type}</span>
+              <div className="avaliation">
+                <span className="material-icons">star</span>
+                <span>{stay.rating}</span>
+              </div>
             </div>
-          </div>
-          <span className="title">Stylist apartment in center of the city</span>
-        </Card>
-
-        <Card>
-          <img src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80" alt="Hotel" />
-          <div className="infos">
-            <span className="host-tag">SUPER HOST</span>
-            <span>Entire apartment . 2 beds</span>
-            <div className="avaliation">
-              <span className="material-icons">star</span>
-              <span>4.40</span>
-            </div>
-          </div>
-          <span className="title">Stylist apartment in center of the city</span>
-        </Card>
-        <Card>
-          <img src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80" alt="Hotel" />
-          <div className="infos">
-            <span className="host-tag">SUPER HOST</span>
-            <span>Entire apartment . 2 beds</span>
-            <div className="avaliation">
-              <span className="material-icons">star</span>
-              <span>4.40</span>
-            </div>
-          </div>
-          <span className="title">Stylist apartment in center of the city</span>
-        </Card>
-        <Card>
-          <img src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80" alt="Hotel" />
-          <div className="infos">
-            <span className="host-tag">SUPER HOST</span>
-            <span>Entire apartment . 2 beds</span>
-            <div className="avaliation">
-              <span className="material-icons">star</span>
-              <span>4.40</span>
-            </div>
-          </div>
-          <span className="title">Stylist apartment in center of the city</span>
-        </Card>
+            <span className="title">{stay.title}</span>
+          </Card>
+        ))}
 
       </CardGroup>
 
